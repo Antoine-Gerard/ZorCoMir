90a3cc6c05741fc7ee5debcc117d3f5d14da5458
Antoine <antoine.gerard@winespace.fr>
2023-07-19 09:57:40 +0200

    Fix typing hints

--- components/select.py
+++ components/select.py
@@ -1,14 +1,15 @@
 import streamlit as st
+from typing import List
 
 def browse_file(file_types = ['xlsx']):
     return st.file_uploader('Choisis ton fichier excel',
                             type = file_types)
 
 
-def select_box(label: str, choices: list[str]):
+def select_box(label: str, choices: List[str]):
     return st.selectbox(label,
                         choices)
 
-def multiselect(label: str, options: list[str]):
+def multiselect(label: str, options: List[str]):
     return st.multiselect(label,
                           options)
